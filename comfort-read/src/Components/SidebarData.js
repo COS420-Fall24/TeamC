6eea038e1e4ffde8f365dbfb7147bf2a3f3c4500
Swhynot99 <sean.whynot1@maine.edu>
2024-10-21 15:02:28 -0400

    Removed base React homepage, icons, unneeded files, and css elements. Added sidebar functionality. Components folder, Sidebar.js, SidebarData.js, Changes to App.css

---
+++ comfort-read/src/Components/SidebarData.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+
+export const SidebarData = [
+    {
+        name: "Home"
+    },
+    {
+        name: "Presets"
+    },
+    {
+        name: "Color"
+    },
+    {
+        name: "Format"
+    },
+    {
+        name: "Extra"
+    },
+    {
+        name: "Import/Export"
+    },
+    {
+        name: "Summarize"
+    }
+];
