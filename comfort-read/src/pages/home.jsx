fa85dd8772df42372e441cb8912b427e053f6499
Jayden LeCorps <jayden.lecorps@maine.edu>
2024-10-21 22:08:32 -0400

    Added pages directory

---
+++ comfort-read/src/pages/home.jsx
@@ -0,0 +1,22 @@
+import logo from './../logo.svg';
+import './../App.css';
+import Sidebar from './../Components/Sidebar';
+import React from "react";
+import {Link} from "react-router-dom";
+export function Home(){
+    return (
+        <>
+
+    <div className="App">
+        <Sidebar></Sidebar>
+        
+        <div className = "content">
+            <div>
+                <h1>ComfortRead Home Page</h1>
+            </div>
+        </div>  
+    </div>
+    
+    </>
+    );
+}
