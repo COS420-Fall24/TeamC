abb7e664656beb0995a68331b21d6a9326ceb85f
Jayden LeCorps <jayden.lecorps@maine.edu>
2024-11-19 18:23:13 -0500

    Removed duplicate file

--- comfort-read/src/Pages/home.jsx
+++ comfort-read/src/Pages/home.jsx
@@ -1,11 +1,12 @@
 import './../App.css';
 import Sidebar from '../Components/Sidebar';
-import UserInput from '../Components/UserInput';
+import UserInput from '../Removed/UserInput';
 import React from "react";
 import {Link} from "react-router-dom";
 import { DarkModeContext, DarkModeProvider } from '../Context/DarkModeContext';
 import {useContext} from 'react';
 import FormattedInput from '../Components/FormattedInput';
+import { Navbar } from 'react-bootstrap';
 
 
 
@@ -17,6 +18,7 @@ export function Home(){
         <>
     
     <div id="App" className = {theme} data-testid="App">
+        
         <Sidebar></Sidebar>
         
         <div id = "content">
