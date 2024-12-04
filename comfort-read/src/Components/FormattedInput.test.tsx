22cf6bdbe4faf16b90f4ea2267cb4c251829b759
Jayden LeCorps <jayden.lecorps@maine.edu>
2024-12-03 19:10:02 -0500

    Made progress on mocking Speech Synthesis API. Need to decide which approach to try next.

--- comfort-read/src/Components/FormattedInput.test.tsx
+++ comfort-read/src/Components/FormattedInput.test.tsx
@@ -10,7 +10,9 @@ import { ExportFileProvider } from "../Context/ExportFileContext";
 import { BrowserRouter } from 'react-router-dom'
 import {HashRouter as Router, Routes, Route} from "react-router-dom";
 import FormattedInput from "./FormattedInput";
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
+import { SpeechRecognition, SpeechSynthesisUtterance} from 'corti';
+
 
 document.execCommand = jest.fn()
 
@@ -18,6 +20,25 @@ describe("Text size and color can be changed", () => {
 
     beforeAll(() => {
         global.document.execCommand = jest.fn();
+        const mockSpeechRecognition = {
+            start: jest.fn(),
+            stop: jest.fn(),
+            addEventListener: jest.fn(),
+            removeEventListener: jest.fn(),
+        };
+        
+        global.SpeechRecognition = jest.fn(() => mockSpeechRecognition);
+        global.webkitSpeechRecognition = jest.fn(() => mockSpeechRecognition);
+        global.SpeechSynthesisUtterance = jest.fn();
+
+        /* Approach 1  Right out the mock with better and better values so that when things that are mocked are called, it actually gets the correct values.*/
+        /* Approach 2 Find an Open Source Library that mocks the speechSynthesis API and use that.*/
+        global.speechSynthesis = {
+            speak: jest.fn(),
+            getVoices: jest.fn().mockReturnValue([])
+        };
+
+        
     });
     
     afterEach(() => {
